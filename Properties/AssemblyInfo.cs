add3720338c2d72d3eb8c65c3846a7229705fef6
Logan Shockley <loganv182@gmail.com>
2023-12-21 20:35:58 -0600

    Added assembly version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -16,5 +16,7 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("945622a5-4786-4c52-87f3-624bc6542a09")]
 
+[assembly: AssemblyVersion(RagdollPlayerMod.Version)]
+[assembly: AssemblyFileVersion(RagdollPlayerMod.Version)]
 [assembly: MelonInfo(typeof(RagdollPlayerMod), "Ragdoll Player", RagdollPlayerMod.Version, "Lakatrazz")]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
